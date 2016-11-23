c6da67aa35c756fc8311334d7c2604330ad22811
Faramaz Pat <faramazpat@gmail.com>
2016-11-23 14:04:12 +0100

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './app/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+    {
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react']
+      }
+    },
+    // {
+    //   test: /\.js$/,
+    //   loaders: ['react-hot', 'babel'],
+    //   include: path.join(__dirname, 'src')
+    // }
+  ]
+  }
+};
